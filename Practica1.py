c7c417f60707663e98fc4db9f6585d30aa8a6333
Bosttra <artur.estany@campus.xaloc.org>
2025-09-17 09:45:25 +0200

    Piyth

---
+++ Practica1.py
@@ -0,0 +1,16 @@
+var1=float(input("introduce un numero: "))
+var2=float(input("introduce un numero: "))
+total_suma=var1+var2
+total_resta=var1-var2
+total_multi=var1*var2
+total_divi=var1/var2
+total_diviEnter=var1//var2
+total_diviRest=var1%var2
+total_poten=var1**var2
+print("la suma dels dos nombres equival a: ", total_suma)
+print("la resta dels dos nombres equival a: ", total_resta)
+print("la multiplicació dels dos nombres equival a: ", total_multi)
+print("la divisió dels dos nombres equival a: ", total_divi)
+print("la divisió en num sencer dels dos nombres equival a: ", total_diviEnter)
+print("el restan de la divisió dels dos nombres equival a: ", total_diviRest)
+print("la potencia del numero 1 elevat al segont equival a:  ", total_poten)
