a985964102a0cb6976cb3b571d8e887b3706cdb1
Bosttra <artur.estany@campus.xaloc.org>
2025-09-19 15:42:51 +0200

    da
    ns

--- Exercicis/Practica1,2.py
+++ Exercicis/Practica1,2.py
@@ -5,7 +5,7 @@ var3=6.8
 print(var1)
 print(var2)
 print(var3)
-var11=B
+#var11=B
 var22=3.4
 var33=5.4
 print(var11)
---
+++ Exercicis/practica5.py
@@ -0,0 +1,17 @@
+var1=input("una paraula si us plau. :)  ")
+var2=input("un altre. :)  ")
+var3=input("un altre. :)  ")
+var4=input("adivina una altre. ;) ")
+var5=input("ultima: ")
+
+
+print(var1+var2+var3+var4+var5)
+print("   ")
+print("   ")
+print("   ")
+print("que passa que no te sentit?")
+print("   ")
+print("   ")
+print("   ")
+print("et deixo una llista de paraules aleatories ----->")
+print(f"{var1}, {var2}, {var3}, {var4}, {var5}. \_(·_·)_/ ")
