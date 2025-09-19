409e3e7d9020160815d08cffbd0edbed523848c8
Bosttra <artur.estany@campus.xaloc.org>
2025-09-19 16:07:43 +0200

    Create practica7.py

---
+++ Exercicis/practica7.py
@@ -0,0 +1,17 @@
+var=float(input"un numero: ")
+var2=float(input("un alter: "))
+tatal_suma=var1+var2
+total_resta=var1-var2
+total_multi=var1*var2
+total_potencia=var1**var2
+total_divi=round(2)var1/var2
+total_diviTot=var1//var2
+total_divirest=var1%var2
+
+print(" suma", tatal_suma)
+print("resta", total_resta)
+print("multi", total_multi)
+print("divi", total_divi)
+print("resta de divi", total_divirest)
+print("divi sencer", total_diviTot)
+print("potencia", total_potencia)
