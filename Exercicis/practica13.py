27773440fa1df24dae52443806c8835124f3667d
Bosttra <artur.estany@campus.xaloc.org>
2025-09-26 15:59:47 +0200

    a

---
+++ Exercicis/practica13.py
@@ -0,0 +1,3 @@
+x=float(input("introdueix valor: "))
+print(" el area del cub es ", 6*(x**2))
+print ("es volum es ", x**3)
---
+++ Exercicis/practica15.py
@@ -0,0 +1,5 @@
+import math
+d=float(input("introdueix diametre: "))
+H=float(input("introdueix altura: "))
+print(f"el perimetre es {math.pi*d*H}")
+print(f"el area es {(math.pi*(d/2)**2)*H}")
