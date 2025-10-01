1341b92c42fcebd1086a38c689b1bf4850afc186
Bosttra <artur.estany@campus.xaloc.org>
2025-10-01 09:20:18 +0200

    Create practica19.py

---
+++ condicionals/exercicis/practica19.py
@@ -0,0 +1,9 @@
+x=float(input("introdueix un numero: "))
+y=float(input("introdueix un numero: "))
+if x==y:
+    print("son iguales")
+else:
+    if x<y:
+        print( y, "es major")
+    else:
+        print(x, "es major")
