609f5ee394b43930d9467ba87cee145624c06b85
Bosttra <artur.estany@campus.xaloc.org>
2025-10-08 09:23:59 +0200

    Create practica28.py

---
+++ condicionals/exercicis/practica28.py
@@ -0,0 +1,13 @@
+#. Mejora el programa anterior para controlar también la introducción de símbolos. Utiliza 
+#elif.
+x=input("introduce una letra en mayuscula o minuscula")
+if x.isnumeric():
+    print("el valor introducido no es correcto")
+elif not x.isalpha():
+     print("el valor introducido no es correcto")
+else:
+    if x.isupper()==True:
+        print("es mayuscula")
+    if not x.isupper()==True:
+      print("es minuscula")
+
