4105b8addc5e3e1eae2cd80a842d098f42221689
Bosttra <artur.estany@campus.xaloc.org>
2025-10-08 09:38:38 +0200

    Create practica30.py

---
+++ condicionals/exercicis/practica30.py
@@ -0,0 +1,10 @@
+# Realiza un programa que controle si la longitud de una frase introducida por teclado es
+#igual, menor o mayor de 11 caracteres. Utiliza elif
+x=input("introdueix una frase: ")
+x=len(x)
+if x<11:
+    print("no te 11 caracters")
+elif x==11:
+    print("te 11 caracters")
+else:
+    print("te mes de 11 caracters")
