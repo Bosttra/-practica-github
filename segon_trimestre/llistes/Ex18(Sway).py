1eb9d3a82b247a48630218b7c656a2f7087edfd7
Bosttra <artur.estany@campus.xaloc.org>
2026-01-28 09:56:04 +0100

    Update Ex18(Sway).py

--- segon_trimestre/llistes/Ex18(Sway).py
+++ segon_trimestre/llistes/Ex18(Sway).py
@@ -1,6 +1,29 @@
 #86. Realiza el ejercicio del DNI que encontrarás en el Sway
-llista_intents=[],llista_nums=[],DNIcorectes=[],DNIincorectes=[],rep=True
+llista_intents=[]
+llista_nums=[]
+DNIcorectes=[]
+DNIincorectes=[]
+rep=True
 lletres=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
 while rep==True:
-    llista_intents.append(input("introdueix els valors numerics del DNI: "))
+    x=input("introdueix els valors numerics del DNI: ")
+    print(sum(int(x)))
+    print(sum(list(x))/23)
+    if len(x)!=8:
+        DNIincorectes.append(x)
+        llista_intents.append(0)
+        print("ERROR; FALTA DE LLARGADA")
+    elif x.isalpha():
+        DNIincorectes.append(x)
+        llista_intents.append(1)
+        print("ERROR; VALORS NO NUMERICS")
+    elif not int(x)/23>=0 or not int(x)/23<=22:
+        DNIincorectes.append(x)
+        llista_intents.append(2)     
+        print("ERROR; DNI INCORRECTE")
+    else:
+        llista_intents.append(3)
+        x+=lletres[int(x)/23]
+        DNIcorectes.append(x)
+        print("DNI ES: ",x)
 
