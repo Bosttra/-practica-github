87239c120c1d6540553e476f9e3b01f2652da85c
Bosttra <artur.estany@campus.xaloc.org>
2026-02-20 16:07:26 +0100

    a

--- segon_trimestre/ahorcado/ahorcado.py
+++ segon_trimestre/ahorcado/ahorcado.py
@@ -1,18 +1,58 @@
 import  random
 Lista_partida=[]
-Lista_ahorcado=""
+#conte el estat de la partida
+
+Lista_ahorcado=["A","H","O","R","C","A","D","O",0]
+#conte els errors de la partida
+
 Lista_palabrasecreta=["maduixa","escombra","llampada","cargol","estelada","piruleta","xarxa","tortuga","bruixot"]
-palabra=list(Lista_palabrasecreta[random.randint(0,len(Lista_palabrasecreta))])
+#conte les paraules
 
-for x in palabra:
-    Lista_partida.append("_" )
+palabra=""
+#conte la paraula a adibinar
 
-print("adivina la paraula")
-print(Lista_partida)
-while len(Lista_ahorcado)==8 or "_" in Lista_partida:
-    x=input("introdueix lletra: ")
-    if x in palabra:
-        while palabra.index(x):
-            Lista_partida.pop(palabra.index(x))
-            Lista_partida.insert(palabra.index(x), x)
+x=""
+#conte la lletra a comprovar
+
+rep=True
+#controla les repeticions
+
+def escollir():
+    palabra=list(Lista_palabrasecreta[random.randint(0,len(Lista_palabrasecreta))])
+    for j in palabra:
+        Lista_partida.append("_" )
+
+
+def joc():
+    while Lista_ahorcado[8]!=8 and "_" in Lista_partida:
+        print(Lista_partida)
+        x=input("introdueix lletra: ")
+        if x in palabra:
+            for j in range(len(palabra)):
+                if palabra[j]==x:
+                    Lista_partida.pop(j)
+                    Lista_partida.insert(j,x)
+            print("Correcte!")
+        else:
+            Lista_ahorcado.append(Lista_ahorcado[8]+1)
+            Lista_ahorcado.pop(8)
+            print("incorrecte!")
+        print(f"errors: {Lista_ahorcado[8]}: ", Lista_ahorcado[0:Lista_ahorcado[8]])
+
+def repetir():
+    rep=input("Vols repetir?")
+    if rep.lower() in "sísi":
+        rep=True
+    elif rep.lower() in "no":
+        print()
+    else:
+        print("Error")
+        repetir()
+        
+while rep==True:
+    escollir()
+    joc()
+    repetir()
+print("adivina la paraula: ")
+
 
